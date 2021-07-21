58a748ebd7af87ad5c8f9930399bb4477c061ba7
Youssef Ismail <yismail@ualberta.ca>
2021-07-21 00:07:05 -0600

    animated

---
+++ index.js
@@ -0,0 +1,15 @@
+let prev = -1;
+let index = -2
+setInterval(() => {
+  do {
+    index = Math.floor(Math.random() * 4);
+  } while (index === prev);
+  prev = index
+  const links = document.querySelectorAll("a");
+  const curr = links[index];
+  console.log(index);
+  curr.style.animationPlayState = "running";
+  setTimeout(() => {
+    curr.style.animationPlayState = "paused";
+  }, 3000);
+}, 3000);
