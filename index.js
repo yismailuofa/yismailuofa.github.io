71a99c8ad9e112d3da132a52a1ef48ca855acfe4
Youssef Ismail <yismail@ualberta.ca>
2021-07-21 00:11:34 -0600

    clg

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 let prev = -1;
-let index = -2
+let index = -2;
 setInterval(() => {
   do {
     index = Math.floor(Math.random() * 4);
   } while (index === prev);
-  prev = index
+  prev = index;
   const links = document.querySelectorAll("a");
   const curr = links[index];
-  console.log(index);
   curr.style.animationPlayState = "running";
   setTimeout(() => {
     curr.style.animationPlayState = "paused";
