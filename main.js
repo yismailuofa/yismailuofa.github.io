71303ef61db29941d251ce5081203217ae581c52
Youssef Ismail <yismail@ualberta.ca>
2024-04-13 01:09:17 -0600

    more updates, maybe cleaner?

--- main.js
+++ main.js
@@ -1,29 +1,20 @@
-const RESOURCES = "resources.json";
+import { RESOURCES } from "./resources.js";
 
-fetch(RESOURCES)
-  .then((r) => r.json())
-  .then(createRows)
-  .catch(console.error);
+const tbody = document.getElementById("link-tdbody");
 
-function createRows(items) {
-  const tbody = document.getElementById("link-tdbody");
+RESOURCES.forEach(({ name, url, author, type }) => {
+  const tr = document.createElement("tr");
+  const tdTitle = document.createElement("td");
+  const tdAuthor = document.createElement("td");
+  const tdType = document.createElement("td");
 
-  items.forEach(({ name, url, author, type }) => {
-    const tr = document.createElement("tr");
-    const tdTitle = document.createElement("td");
-    const tdAuthor = document.createElement("td");
-    const tdType = document.createElement("td");
+  tdTitle.innerHTML = `<a href="${url}" target="_blank">${name}</a>`;
+  tdAuthor.innerText = author;
+  tdType.innerText = type;
 
-    tdTitle.innerHTML = `<a href="${url}" target="_blank">${name}</a>`;
-    tdAuthor.innerText = author;
-    tdType.innerText = type;
+  tr.appendChild(tdTitle);
+  tr.appendChild(tdType);
+  tr.appendChild(tdAuthor);
 
-    tr.appendChild(tdTitle);
-    tr.appendChild(tdType);
-    tr.appendChild(tdAuthor);
-
-    tbody.appendChild(tr);
-  });
-
-  return tbody;
-}
+  tbody.appendChild(tr);
+});
